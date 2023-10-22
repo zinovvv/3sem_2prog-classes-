b8473aa144f2668bfc871b22908863578d06c1f5
Zinov <artem.zinov.16@gmail.com>
2023-10-22 14:20:53 +0700

    Написал методы для классов Track,Album,Playlist

--- 3sem_2prog(classes)/main.cpp
+++ 3sem_2prog(classes)/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <Windows.h>
 
 class Track {
 private:
@@ -18,7 +19,6 @@ public:
 
 
     static Track InputTrack() {
-
         std::string title, artist, genre;
         int duration, year;
 
@@ -36,6 +36,7 @@ public:
 
         std::cout << "Год выхода трека: ";
         std::cin >> year;
+
 
         return Track(title, artist, duration, year, genre);
     }
@@ -89,6 +90,21 @@ public:
         }
         return Album(title, artist, year, num_tracks);
     }
+
+    void SetTracks(const std::vector<Track>& newTracks) {
+        tracks = newTracks;
+    }
+
+    void OutputAlbum() const {
+        std::cout << "Название: " << title << std::endl;
+        std::cout << "Исполнитель: " << artist << std::endl;
+        std::cout << "Год выхода: " << year << std::endl;
+        std::cout << "Кол-во треков: " << num_tracks << std::endl;
+        for (const Track& track : tracks) {
+            track.OutputTrack();
+        }
+    }
+
     ~Album() {}
 };
 
@@ -130,8 +146,42 @@ public:
         tracks.push_back(track);
     }
 
+    void SetTracks(const std::vector<Track>& newTracks) {
+        tracks = newTracks;
+    }
+
+    void PrintTracks() const {
+        for (const Track& track : tracks) {
+            track.OutputTrack();
+            std::cout << std::endl;
+        }
+    }
+
+    void DeleteTrack() {
+        int number;
+        std::cout << "Введите номер трека, который хотите удалить:";
+        std::cin >> number;
+        tracks.erase(tracks.begin() + number-1);
+    }
+
+    void InputTrack() {
+            Track track = Track::InputTrack();
+            tracks.push_back(track);
+    }
+
     void AddAlbum(Album& album) {
         albums.push_back(album);
+    }
+
+    void SetAlbums(const std::vector<Album>& newAlbums) {
+        albums = newAlbums;
+    }
+
+    void PrintAlbums() const {
+        for (const Album& album : albums) {
+            album.OutputAlbum();
+            std::cout << std::endl;
+        }
     }
 
     ~Playlist(){}
@@ -163,6 +213,67 @@ public:
 };
 
 int main() {
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "rus");
+
+    MusicLibrary myMusicLibrary(1, 1);
+
+    Playlist myPlaylist("Любимые треки", 3, 2);
+    std::vector<Track> tracks = {
+    {"Кукушка", "Виктор Цой", 400, 1986, "рок"},
+    {"Метель", "Би-2", 345, 1998, "рок"},
+    {"Лето в городе", "Иван Дорн", 328, 2012, "поп"}
+    };
+    myPlaylist.SetTracks(tracks);
+    myPlaylist.PrintTracks();
+    //myPlaylist.DeleteTrack();
+    //myPlaylist.PrintTracks();
+    //myPlaylist.InputTrack();
+    //myPlaylist.PrintTracks();
+
+    Album album1("Несчастные люди", "ЛСП", 2023, 12);
+    std::vector<Track> tracks1 = {
+        {"Карантин", "ЛСП", 180, 2023, "поп хип-хоп"},
+        {"Троянский конь", "ЛСП", 200, 2023, "поп хип-хоп"},
+        {"Монополия", "ЛСП", 198, 2023, "поп хип-хоп"},
+        {"Золотой ключик", "ЛСП", 222, 2023, "поп хип-хоп"},
+        {"Несчастные люди", "ЛСП", 210, 2023, "поп хип-хоп"},
+        {"Вечная весна", "ЛСП", 195, 2023, "поп хип-хоп"},
+        {"Ураганы", "ЛСП", 189, 2023, "поп хип-хоп"},
+        {"Не убежал", "ЛСП", 240, 2023, "поп хип-хоп"},
+        {"Боди", "ЛСП", 200, 2023, "поп хип-хоп"},
+        {"Сектор Приз", "ЛСП", 192, 2023, "поп хип-хоп"},
+        {"Сикрет Плейс", "ЛСП", 203, 2023, "поп хип-хоп"},
+        {"Стоп Игра", "ЛСП", 240, 2023, "поп хип-хоп"}
+    };
+    album1.SetTracks(tracks1);
+
+    Album album2("Дух мира", "Джизус", 2023, 13);
+    std::vector<Track> tracks2 = {
+         {"Я голоден", "Джизус", 171, 2023, "альтернатива"},
+        {"Едкий дым", "Джизус", 224, 2023, "альтернатива"},
+        {"Маньяк", "Джизус", 161, 2023, "альтернатива"},
+        {"Галактика", "Джизус", 330, 2023, "альтернатива"},
+        {"Всё забрать", "Джизус", 239, 2023, "альтернатива"},
+        {"Плавишься", "Джизус", 167, 2023, "альтернатива"},
+        {"Ждал тебя", "Джизус", 225, 2023, "альтернатива"},
+        {"Рай или Ад", "Джизус", 258, 2023, "альтернатива"},
+        {"Spirit of the World", "Джизус", 217, 2023, "альтернатива"},
+        {"Жвачка", "Джизус", 266, 2023, "альтернатива"},
+        {"Заповедь", "Джизус", 97, 2023, "альтернатива"},
+        {"Сигареты и творчество", "Джизус", 155, 2023, "альтернатива"},
+        {"Каплей дождя", "Джизус", 238, 2023, "альтернатива"}
+    };
+    album2.SetTracks(tracks2);
+
+    std::vector<Album> albums = { {album1},{album2} };
+
+    myPlaylist.SetAlbums(albums);
+    myPlaylist.PrintAlbums();
+
+    /*ml = DeletePlaylistAlbum(ml);*/
+    //ml = AlbumInputPlaylist(ml);
+    /*OutputPlaylistAlbums(ml);*/
 
     return 0;
 }
